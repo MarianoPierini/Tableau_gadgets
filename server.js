49faca7997d62e217ec9abe75a40e9522a2d2da2
MarianoPierini <134010310+MarianoPierini@users.noreply.github.com>
2026-02-09 18:03:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,26 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SECRET_TOKEN = process.env.SECRET_TOKEN || 'aero2025gauge';
 
-app.use(express.static(__dirname));
+// Middleware de validación
+function checkToken(req, res, next) {
+  if (req.query.token === SECRET_TOKEN) {
+    next();
+  } else {
+    res.status(403).send('Acceso denegado');
+  }
+}
 
-app.get('/', (req, res) => {
+app.get('/', checkToken, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/configure', (req, res) => {
+app.get('/configure', checkToken, (req, res) => {
   res.sendFile(path.join(__dirname, 'configure.html'));
 });
+
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
   console.log(`Premium Gauge running on port ${PORT}`);
